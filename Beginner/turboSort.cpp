1d88882eeed93e32850aa0f219cf5905e233ecc3
Anurag Pal <apal895@gmail.com>
2020-06-01 11:35:06 +0530

    Turbo Sort Done

--- Beginner/turboSort.cpp
+++ Beginner/turboSort.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main()
 {
     int n;
+    cin >> n;
+    long int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << endl
+             << a[i];
+    }
 
     return 0;
 }
