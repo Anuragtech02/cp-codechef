7b3f52e38ea5b892544ce1ac210e29879054ab10
Anurag Pal <apal895@gmail.com>
2020-06-12 13:48:44 +0530

    FSQRT Completed

---
+++ Beginner/FSQRT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int number;
+        cin >> number;
+        cout << (int)sqrt(number) << endl;
+    }
+}
