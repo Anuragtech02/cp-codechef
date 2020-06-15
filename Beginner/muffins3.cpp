65e6f1c2e5f1d9b66ae31820c62f72072c70adbf
Anurag Pal <apal895@gmail.com>
2020-06-15 18:53:48 +0530

    MuffinS3 Done

---
+++ Beginner/muffins3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        long int a;
+        cin >> a;
+        cout << a / 2 + 1 << endl;
+    }
+    return 0;
+}
