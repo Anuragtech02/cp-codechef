58ca0f6f97d667bd4d8ec5d7347e8c3ea667b32d
Anurag Pal <apal895@gmail.com>
2020-06-19 14:53:32 +0530

    XYSTR Complete after two updates

--- june-long-challenge-2020/xystr.cpp
+++ june-long-challenge-2020/xystr.cpp
@@ -8,11 +8,11 @@ int main()
     while (n--)
     {
         int count = 0;
-        string str;
+        char str[100000];
         cin >> str;
-        for (int i = 0; i < str.length();)
+        for (int i = 0; str[i] != '\0';)
         {
-            if (str[i] == 'x' && str[i + 1] == 'y' || str[i] == 'y' && str[i + 1] == 'x')
+            if ((str[i] == 'x' && str[i + 1] == 'y') || (str[i] == 'y' && str[i + 1] == 'x'))
             {
                 count++;
                 i += 2;
