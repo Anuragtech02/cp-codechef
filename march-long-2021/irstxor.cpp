b487912c5bd22cd85e4bfb50a8c193637eb70b3d
Anuragtech02 <apal895@gmail.com>
2021-03-09 18:39:32 +0530

    irstxor done

--- march-long-2021/irstxor.cpp
+++ march-long-2021/irstxor.cpp
@@ -7,30 +7,10 @@ int main()
     cin >> t;
     while (t--)
     {
-        long long int n, max = 1;
+        long long int n;
         cin >> n;
-        int a, b;
-        // int d = log2(n);
-        // cout << d << "\n";
         long long int dest = pow(2, (int(log2(n)) + 1));
-        // cout << "\ndest" << dest << "\n";
-        for (long int i = 1; i < dest; i++)
-        {
-            for (long int j = i; j < dest; j++)
-            {
-                if ((i ^ j) == n)
-                {
-                    long long int tempProd = i * j;
-                    if (tempProd > max)
-                    {
-                        max = tempProd;
-                        a = i;
-                        b = j;
-                    }
-                }
-            }
-        }
-        cout << max << "\n"
-             << "a " << a << "b " << b << endl;
+        long long a = (dest / 2) - 1, b = a ^ n;
+        cout << a * b << "\n";
     }
 }
