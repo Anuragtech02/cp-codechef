021e1fd7d0ad7b786e85de4fef455aa07626124b
Anuragtech02 <apal895@gmail.com>
2021-03-10 18:26:47 +0530

    spacearr done

--- march-long-2021/spacearr.cpp
+++ march-long-2021/spacearr.cpp
@@ -7,45 +7,29 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n, flag = 0, res = 0;
+        int n, flag = 0, currTotal = 0;
         cin >> n;
         int series[n];
         for (int i = 0; i < n; i++)
         {
             cin >> series[i];
         }
-        if (n == 1)
+        sort(series, series + n);
+        for (int i = 0; i < n; i++)
         {
-            flag = 1;
-            res = 1;
+            if (series[i] > (i + 1))
+                flag = 1;
+            currTotal += series[i];
+        }
+        if (!flag)
+        {
+            int nTotal = n * (n + 1) / 2;
+            if ((nTotal - currTotal) % 2)
+                cout << "First\n";
+            else
+                cout << "Second\n";
         }
         else
-        {
-            while (!flag)
-            {
-
-                // cout << "First : " << series[0] << " Second " << series[1] << "\n";
-                if (series[0] > n)
-                {
-                    flag = 1;
-                    res = 2;
-                }
-                else if (series[1] > n)
-                {
-                    flag = 1;
-                    res = 1;
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    series[i] += 1;
-                    // cout << series[i] << " ";
-                }
-            }
-        }
-
-        if (res == 1)
-            cout << "First\n";
-        else if (res == 2)
             cout << "Second\n";
     }
-}   
+}
